faf83ddb10cf84bf2a68ad0341bf7794ccf66811
Venugopal Sopeti <venugopal.sopeti@gmail.com>
2026-05-14 00:46:01 +0530

    css path updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://VenugopalSopeti.github.io",
-  base: "/myprofile-app",
+  base: "/myprofile",
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()]
